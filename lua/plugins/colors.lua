72337b76f2f4d6ee767b251e03507a0e12bf8dd2
Francisco Avila Luna <devpacos365200420@gmail.com>
2024-08-28 12:28:31 -0600

    Update default colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,5 +1,6 @@
 function ColorMyPencils(color)
-    color = color or "rose-pine-moon"
+    -- color = color or "rose-pine-moon"
+    color = color or "lunaperche"
     vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -19,3 +20,4 @@ return {
         end
     }
 }
+
