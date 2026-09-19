ba25f1df3da283d2eb678018cfd021787eebcfe4
Francisco <devpacos365200420@gmail.com>
2026-09-19 12:30:24 -0600

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").install({
+      "python", "lua", "c", "cpp", "java", "sql",
+      "html", "css", "javascript", "typst",
+      "markdown", "markdown_inline", "bash",
+      "json", "yaml", "toml", "vim", "vimdoc", "query",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "python", "lua", "c", "cpp", "java", "sql",
+        "html", "css", "javascript", "typst",
+        "markdown", "bash", "json", "yaml", "toml",
+      },
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.wo.foldmethod = "expr"
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
