448f9f7f40125ce145062d1640c9eb983c1ee25c
Francisco Avila Luna <113661168+franciscoavilaluna@users.noreply.github.com>
2026-09-23 23:42:03 -0600

    Enhance mermaid.nvim setup with preview options

--- lua/plugins/mermaid.lua
+++ lua/plugins/mermaid.lua
@@ -2,6 +2,12 @@ return {
     "kevalin/mermaid.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     config = function()
-        require("mermaid").setup()
+        require("mermaid").setup({
+            preview = {
+                port = 0,
+                renderer = "mermaid.js",
+                theme = "dark",
+            },
+        })
     end,
 }
