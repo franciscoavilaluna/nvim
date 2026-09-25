bfb21af47346eeca001713860f9766f64d8a646e
Francisco Avila Luna <113661168+franciscoavilaluna@users.noreply.github.com>
2026-09-24 23:39:02 -0600

    Add MATLAB support to Treesitter configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,14 +7,14 @@ return {
       "python", "lua", "c", "cpp", "java", "sql",
       "html", "css", "javascript", "typst",
       "markdown", "markdown_inline", "bash",
-      "json", "yaml", "toml", "vim", "vimdoc", "query", "mermaid"
+      "json", "yaml", "toml", "vim", "vimdoc", "query", "mermaid", "matlab"
     })
 
     vim.api.nvim_create_autocmd("FileType", {
       pattern = {
         "python", "lua", "c", "cpp", "java", "sql",
         "html", "css", "javascript", "typst",
-        "markdown", "bash", "json", "yaml", "toml", "mermaid"
+        "markdown", "bash", "json", "yaml", "toml", "mermaid", "matlab"
       },
       callback = function()
         pcall(vim.treesitter.start)
